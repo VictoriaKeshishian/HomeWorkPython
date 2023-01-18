2b17b38c357a50b59b7a08ed509cdd4d7d4bc798
Victoria Keshishian <viky_lechka@mail.ru>
2023-01-18 13:40:58 +0300

    Задача абв

--- Seminar5/task1.py
+++ Seminar5/task1.py
@@ -1,2 +1,8 @@
 # 38. Напишите программу, удаляющую из текста все слова, содержащие ""абв""
 
+text = 'Высокий уровень вовлечабв представителей целевой аудитории является четкабв дабввлений прогрессивного развития'
+print(text)
+list_text = text.split(sep = ' ')
+find_txt = "абв"
+result = list(filter(lambda x: True if find_txt not in x else False, list_text))
+print(result)
